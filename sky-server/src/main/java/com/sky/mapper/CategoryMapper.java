0b97a51523560bf03df7bb82542fd8d2ea48d850
ezlier <86654833@qq.com>
2026-02-03 16:55:55 +0800

    新增菜品

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -2,13 +2,12 @@ package com.sky.mapper;
 
 import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
-import com.sky.enumeration.OperationType;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
 @Mapper
@@ -19,8 +18,8 @@ public interface CategoryMapper {
      * @param category
      */
     @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
-    "VALUES" +
-    "(#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+            " VALUES" +
+            " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     @AutoFill(value = OperationType.INSERT)
     void insert(Category category);
 
@@ -35,13 +34,14 @@ public interface CategoryMapper {
      * 根据id删除分类
      * @param id
      */
-    void deleteById(@Param("id") Long id);
+    @Delete("delete from category where id = #{id}")
+    void deleteById(Long id);
 
     /**
      * 根据id修改分类
      * @param category
      */
-    @AutoFill(OperationType.UPDATE)
+    @AutoFill(value = OperationType.UPDATE)
     void update(Category category);
 
     /**
